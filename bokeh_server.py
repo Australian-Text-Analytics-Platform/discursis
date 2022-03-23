aefac8ce36b410f95a6edbbb7139279f5b931853
Marius <marius.mather@sydney.edu.au>
2022-03-23 13:32:54 +1100

    Remove bokeh_server script, not needed

--- bokeh_server.py
+++
@@ -1,8 +0,0 @@
-from subprocess import Popen
-
-def _load_jupyter_server_extension(serverapp: 'jupyter_server.serverapp.ServerApp'):
-    """
-    This function is called when the extension is loaded.
-    """
-    Popen(["bokeh", "serve", "bokeh-app", "--allow-websocket-origin=*"])
-    
