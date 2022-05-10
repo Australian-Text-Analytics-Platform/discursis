97fe8b41b968f5a8a49b8e1395f441f1e27920aa
Marius <marius.mather@sydney.edu.au>
2022-05-10 15:29:39 +1000

    Clean up unneeded config files

--- install.R
+++
@@ -1,9 +0,0 @@
-# install packages
-install.packages("tidyverse")
-install.packages("boot")
-install.packages("DescTools")
-install.packages("tidyverse")
-install.packages("psych")
-install.packages("Rmisc")
-install.packages("ggpubr")
-install.packages("flextable")
